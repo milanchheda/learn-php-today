35328b831daf0ee80953ac2e55b8440dd791c2e9
milanchheda <milan.chheda@gmail.com>
2017-06-12 15:26:46 +0400

    Added GA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="Description" content="LearnPHPToday is a news & feeds related to PHP. Learn something new everyday."/>
+    <meta name="Keywords" content="Learn PHP, PHP, PHP7, PHP5.6, Laravel, Learn PHP Today, PHP Today"/>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Learn PHP Today</title>
 
-    
     <link href="{{ mix('css/custom.min.css') }}" rel="stylesheet">
     
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -24,6 +24,21 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
+    <?php
+    if(env('APP_ENV') != 'local') {
+    ?>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-100841850-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <?php
+    }
+    ?>
 </head>
 <body>
     <div id="app">
