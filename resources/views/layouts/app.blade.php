6ef35e587ed65539ec0cd03daf1da1081f7f3ec4
milanchheda <milan.chheda@gmail.com>
2017-07-13 10:11:19 +0400

    Removed form.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,12 +140,10 @@
                     
                     <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
                     
-                    <div id="mc_embed_signup">
+                    <!-- <div id="mc_embed_signup">
                         <form action="//learnphptoday.us16.list-manage.com/subscribe/post?u=48ed5ee6722b8f1018036ed76&amp;id=426dfb94e2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                             <div id="mc_embed_signup_scroll">
-                                <!-- <label for="mce-EMAIL">Subscribe to our mailing list</label> -->
                                 <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-                                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                                 <div style="position: absolute; left: -5000px;" aria-hidden="true">
                                     <input type="text" name="b_48ed5ee6722b8f1018036ed76_426dfb94e2" tabindex="-1" value="">
                                 </div>
@@ -154,7 +152,7 @@
                                 </div>
                             </div>
                         </form>
-                    </div>
+                    </div> -->
 
                     <!--End mc_embed_signup-->
                     <!-- <div style="min-height:70px;">
